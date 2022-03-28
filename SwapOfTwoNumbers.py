11735f20986a643f0eb44a6e966339c2aef65b98
Nishar Ahamed H <91793825+Nishar-ahamed-h@users.noreply.github.com>
2022-03-28 21:54:07 +0530

    Add files via upload

---
+++ SwapOfTwoNumbers.py
@@ -0,0 +1,7 @@
+P = int( input("Enter value for P: "))  
+Q = int( input("Enter value for Q: "))  
+temp_1 = P  
+P = Q  
+Q = temp_1  
+print ("The Value of P after swapping: ", P)  
+print ("The Value of Q after swapping: ", Q)  
