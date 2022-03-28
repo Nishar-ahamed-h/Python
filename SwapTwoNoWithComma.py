0085fa89c0f71851585bf7530737145e85dcf4e8
Nishar Ahamed H <91793825+Nishar-ahamed-h@users.noreply.github.com>
2022-03-28 21:58:11 +0530

    Add files via upload

---
+++ SwapTwoNoWithComma.py
@@ -0,0 +1,5 @@
+P = int( input("Please enter value for P: "))  
+Q = int( input("Please enter value for Q: "))  
+P, Q = Q, P     
+print ("The Value of P after swapping: ", P)  
+print ("The Value of Q after swapping: ", Q)  
